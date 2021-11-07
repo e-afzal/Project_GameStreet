df75f682e6332bd104de80b00b423342d576278e
Essam Afzal <essam.afzal@outlook.com>
2021-11-08 05:17:14 +0530

    Added path import to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import colors from "colors";
+import path from "path";
 
 // ROUTES IMPORT
 import productRoutes from "./routes/productRoutes.js";
